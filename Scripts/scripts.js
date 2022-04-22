ed5d8c1e58aa252ad82817970f18c6e3c336c9cc
JaxyBLU <tukebaike123@gmail.com>
2022-04-23 02:41:41 +0400

    Added footer of the popup menu, added esc key closing function for the popup menu

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -5,6 +5,11 @@ const popupclose = document.getElementById('popup-close');
 /* Navigation button which opens popup menu */
 navbutton.addEventListener('click', function(e){
     e.preventDefault();
+    window.onkeydown = function( event ) {
+        if ( event.keyCode == 27 ) {
+            popupmenu.style.visibility = 'hidden';
+        }
+    };
     popupmenu.style.visibility = 'visible';
 })
 
