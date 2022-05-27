2e024721e208fb86e1bf49d9ff421cbf94b1bd06
JaxyBLU <tukebaike123@gmail.com>
2022-05-27 22:46:21 +0400

    Now everything is working without jQuery and on GitHub Pages as well, currently working on calculating final standart price of the choosen model

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -8,8 +8,9 @@ const searchButton = document.getElementById('search-button');
 const inputForm = document.getElementById('search-form');
 const yearNow = 2022;
 
+/* Data for the models selection */
 const bmwmodels = `
-<option value="M5Competition">M5 Competition</option>
+<option value="M5Competition">M5Competition</option>
 <option value="3Series">3Series</option>
 <option value="5Series">5Series</option>
 <option value="7Series">7Series</option>
@@ -44,6 +45,44 @@ const toyotamodels = `
 <option value="Highlander">Highlander</option>
 `;
 
+/* Data for the prices */
+const bmwprices = {
+    "M5Competition": 150000,
+    "3Series": 1300,
+    "5Series": 1500,
+    "7Series": 1700,
+    "X5": 15000,
+    "X7": 17000
+};
+
+const hondaprices = {
+    "Accord": 1888,
+    "CR-V": 1999,
+    "Civic": 2111
+};
+
+const subaruprices = {
+    "Forester": 6666,
+    "Impreza": 7777,
+    "Outback": 8888
+};
+
+const nissanprices = {
+    "Pathfinder": 9876,
+    "Teana": 8765,
+    "Primera": 7654
+};
+
+const toyotaprices = {
+    "Camry": 12345,
+    "Avensis": 23456,
+    "Allion": 34567,
+    "Corolla": 45678,
+    "RAV4": 56789,
+    "Highlander": 7890
+};
+
+/* Models being changed depending on the selected make of the vehicle */
 makeInput.addEventListener('change', function(){
     const makechoosen = makeInput.value;
     switch(makechoosen){
@@ -69,7 +108,9 @@ makeInput.addEventListener('change', function(){
 inputForm.addEventListener('submit', async function(e){
     e.preventDefault();
     alert('Form Submitted!');
-    console.log(collectInput());
+    const datanow = collectInput();
+    const priceofthemodel = getPriceForModel(datanow.make, datanow.model);
+    const finalprice = calculateFinalPrice(datanow, priceofthemodel);
 })
 
 /* Function for collecting input data */
@@ -85,3 +126,31 @@ function collectInput(){
     return inputData;
 }
 
+/* Function for getting the price for the selected model */
+function getPriceForModel(make, model){
+    var price = 0;
+    switch(make){
+        case "BMW":
+            price = bmwprices[model];
+            break;
+        case "Honda":
+            price = hondaprices[model];
+            break;
+        case "Subaru":
+            price = subaruprices[model];
+            break;
+        case "Nissan":
+            price = nissanprices[model];
+            break;
+        case "Toyota":
+            price = toyotaprices[model];
+            break;
+    }
+
+    return price;
+}
+
+/* Function for calculating final price for the selected model and characteristics */
+function calculateFinalPrice(datanow, priceofthemodel){
+    
+}
